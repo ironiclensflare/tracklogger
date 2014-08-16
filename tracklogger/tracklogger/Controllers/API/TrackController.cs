3a9c9cb156d5fe390b1816a7d56f3364da85291a
Stu <stu@ironiclensflare.com>
2014-08-16 11:37:54 +0100

    Added API controller

---
+++ tracklogger/tracklogger/Controllers/API/TrackController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using tracklogger.Models;
+
+namespace tracklogger.Controllers.API
+{
+    public class TrackController : ApiController
+    {
+        private TrackPointsContext tc = new TrackPointsContext();
+
+        public HttpResponseMessage Get(decimal lat, decimal lng, decimal ele, decimal crs, decimal spd, int sat)
+        {
+            TrackPoint t = new TrackPoint();
+            t.Lat = lat;
+            t.Lng = lng;
+            t.Satellites = sat;
+            t.Elevation = ele;
+            t.Course = crs;
+            t.Speed = spd;
+            t.Time = DateTime.Now;
+
+            tc.TrackPoints.Add(t);
+            tc.SaveChanges();
+            
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+    }
+}
